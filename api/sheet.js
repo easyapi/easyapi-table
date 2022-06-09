bc45388afd92a722e6d96d9e6340b91eab0f52be
leida <zhang20084@126.com>
2022-06-09 11:05:12 +0800

    fix:获取数据表详情

---
+++ api/sheet.js
@@ -0,0 +1,15 @@
+import {tableUrl} from './api'
+
+/**
+ * 获取数据表详情
+ *
+ * @param projectId 项目ID
+ * @param sheetId 数据表ID
+ *
+ * @see https://www.easyapi.com
+ */
+export const getSheet = (projectId, sheetId, context) => {
+  return context.$axios.get(`${tableUrl}/${projectId}/sheet/${sheetId}`, {params: {}})
+}
+
+
