c9da1c6b6d8def726267d62ccf5158f09cbddb43
leida <zhang20084@126.com>
2022-11-09 10:00:23 +0800

    fix:根据sheetId获取数据表详情

--- api/sheet.js
+++ api/sheet.js
@@ -12,3 +12,16 @@ import { tableUrl } from './api'
 export const getSheet = (params, teamUrl, projectCode, sheetCode, context) => {
   return context.$axios.get(`${tableUrl}/${teamUrl}/${projectCode}/sheet/${sheetCode}`, { params: { params } })
 }
+
+/**
+ * 根据sheetId获取数据表详情
+ *
+ * @param teamUrl EasyAPI团队URL
+ * @param projectCode EasyAPI项目编码
+ * @param sheetId 数据表ID
+ *
+ * @see https://www.easyapi.com
+ */
+export const getSheetById = (params, teamUrl, projectCode, sheetId, context) => {
+  return context.$axios.get(`${tableUrl}/${teamUrl}/${projectCode}/sheet/id-${sheetCode}`, { params: { params } })
+}
