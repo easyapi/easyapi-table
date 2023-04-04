e522437d7fd9bccecf8a579b5104f8b6a987b930
leida <zhang20084@126.com>
2023-04-04 17:08:55 +0800

    fix:增加route.query.url

--- pages/access-token/index.vue
+++ pages/access-token/index.vue
@@ -2,6 +2,7 @@
 import { onMounted } from 'vue'
 import { useRoute, useRouter } from 'nuxt/app'
 import { settingStore } from '@/stores/setting'
+
 const store = settingStore()
 const route = useRoute()
 const router = useRouter()
@@ -28,8 +29,14 @@ onMounted(() => {
     })
   }
 
-  const path = `/${route.query.teamUrl}/${route.query.projectCode}/${route.query.sheetCode}`
-  router.push(path)
+  if (route.query.url) {
+    router.push(route.query.url as string)
+  }
+  else
+  {
+    const path = `/${route.query.teamUrl}/${route.query.projectCode}/${route.query.sheetCode}`
+    router.push(path)
+  }
 })
 </script>
 
