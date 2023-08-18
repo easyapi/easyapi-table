13515e183e63eb094042781934ee7a7476efef21
leida <zhang20084@126.com>
2023-08-18 10:21:19 +0800

    build:第三方包升级

--- utils/token.ts
+++ utils/token.ts
@@ -1,28 +1,24 @@
-import { useCookies } from '@vueuse/integrations/useCookies'
+import { useCookie } from '#app/composables/cookie'
 
 const key = 'authenticationToken'
-const cookies = useCookies()
 
 /**
  * 获取token
  */
 export function getToken() {
-  return cookies.get(key)
+  return useCookie(key).value
 }
 
 /**
  * 存储token
  */
-export function setToken(token: string) {
-  return cookies.set(key, token, {
-    maxAge: 60 * 60 * 24 * 30,
-    path: '/',
-  })
+export function setToken(token: string): void {
+  useCookie(key, { maxAge: 60 * 60 * 24 * 30, path: '/'}).value = token
 }
 
 /**
  * 移除token
  */
-export function removeToken() {
-  return cookies.remove(key)
+export function removeToken(): void {
+  useCookie(key).value = null
 }
